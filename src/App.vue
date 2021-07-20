52468ed8b311277cc399af821dd6b0edb58e3038
johnson <you@example.com>
2021-07-20 18:16:39 +0800

    框架搭建 axios element-ui less

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>学vue仓库</h1>
+      <el-button>默认按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+      <router-view/>
+
     </div>
-<!--    路由出口 也就是对应上面的路由要显示的组件 -->
-    <router-view/>
+    <!--    路由出口 也就是对应上面的路由要显示的组件 -->
+
   </div>
 </template>
+<script>
+export default {
+  name: 'App'
 
+}
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // 懒加载 访问该路由的时候，组件才会被加载 about.js才会被请求
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// 路由配置表
+const routes = []
 
 const router = new VueRouter({
   routes
